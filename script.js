65ac1023e6a8289a4257a88ff51f737688229a8b
Abdullahkhan871 <123239280+Abdullahkhan871@users.noreply.github.com>
2024-12-15 22:48:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let colorSelect = document.querySelector("#colorSelect");
+      let eleCount = colorSelect.children;
+      let btn = document.querySelector('input[type="button"]');
+
+      btn.addEventListener("click", removeOption);
+
+      function removeOption() {
+        for (let t of eleCount) {
+          if (t.innerHTML == colorSelect.value) {
+            t.remove();
+          }
+        }
+      }
